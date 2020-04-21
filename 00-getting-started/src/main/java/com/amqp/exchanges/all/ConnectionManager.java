efc453fc02daee5d752fb750ff49428fd29b72ca
xiang0818 <944855832@qq.com>
2020-04-21 23:17:26 +0800

    change the url

--- 00-getting-started/src/main/java/com/amqp/exchanges/all/ConnectionManager.java
+++ 00-getting-started/src/main/java/com/amqp/exchanges/all/ConnectionManager.java
@@ -4,6 +4,9 @@ import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 
 import java.io.IOException;
+import java.net.URISyntaxException;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
 import java.util.concurrent.TimeoutException;
 
 public class ConnectionManager {
@@ -19,8 +22,9 @@ public class ConnectionManager {
     if (connection == null) {
       try {
         ConnectionFactory connectionFactory = new ConnectionFactory();
-        connection = connectionFactory.newConnection("amqp://guest:guest@localhost:5672/");
-      } catch (IOException | TimeoutException e) {
+	      connectionFactory.setUri("amqp://user:password@host:5672");
+        connection = connectionFactory.newConnection();
+      } catch (IOException | TimeoutException | URISyntaxException | NoSuchAlgorithmException | KeyManagementException e) {
         e.printStackTrace();
       }
     }
